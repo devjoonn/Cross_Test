731665af31d0059c745aa3ece7ed41b1fb175f4d
devjoonn <phj3046@gmail.com>
2024-03-18 12:58:08 +0900

    Project init

---
+++ Cross_Test/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Cross_Test
+//
+//  Created by 박현준 on 3/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
